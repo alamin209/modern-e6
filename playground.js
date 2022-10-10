71d6d1aa6836e4fa9bc388cb248057cfefc48918
alamin hossain <alamin20192019@gmail.com>
2022-10-10 18:39:45 +0600

    Half done with array funcation

--- playground.js
+++ playground.js
@@ -2,10 +2,30 @@ function testNumber() {
   return 10;
 }
 
-console.log(testNumber());
+// console.log(testNumber());
 
 //E6 Array Function
 let testNumber1 = () => {
   return 10;
 };
-console.log(testNumber1());
+// console.log(testNumber1());
+
+//object iteration
+
+var person = {
+  name: "javascript",
+  library: ["angular", "React", "Vue"],
+  printLibrary: function () {
+    // Normal array will not give the undefine fro name but fate array function will give the correct name as it is an advantage of fat array
+    this.library.forEach((a) => {
+      console.log(`${this.name} love ${a}`);
+    });
+  },
+};
+
+person.printLibrary();
+
+//fate array function example in node
+const search = document.querySelector(".search");
+const result = document.querySelector(".result");
+const thankyou = document.querySelector(".thankyou");
